202ebc2711804200e1fc41e70a4539454278d94e
vivian jelinek <viviancjelinek@gmail.com>
2023-07-18 09:36:05 -0500

    Initial Commit

---
+++ github123/github123App.swift
@@ -0,0 +1,17 @@
+//
+//  github123App.swift
+//  github123
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct github123App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
